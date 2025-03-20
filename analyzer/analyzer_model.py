6623c8cb769b51ba4147bd82103d2f78a0c18a6b
Maks6666 <maxim.kucher2005@gmail.com>
2025-03-20 10:56:45 +0200

    DeepSort implementation

--- analyzer/analyzer_model.py
+++ analyzer/analyzer_model.py
@@ -6,6 +6,6 @@ import numpy as np
 
 model = KNeighborsClassifier()
 model = joblib.load("/Users/maxkucher/opencv/howitzer_detector/analyzer/decision_maker.pkl")
-
-print("Analyzer model loaded")
+params = model.get_params()
+print(f"Analyzer model loaded: {params}")
 
--- deepsort/main.py
+++ deepsort/main.py
@@ -1,4 +1,5 @@
 import cv2
+from future.backports.html.parser import tagfind
 from toolz.curried import peekn
 
 from sort import Sort
@@ -12,7 +13,7 @@ import numpy as np
 
 
 class DeepDetector:
-    def __init__(self, path, device, threshold, model_path, yolo_model_path):
+    def __init__(self, path, device, threshold, detection_area, frame_size, model_path, yolo_model_path):
         self.path = path
         self.device = device
         self.threshold = threshold
@@ -29,6 +30,9 @@ class DeepDetector:
 
         self.feature_list = ["person", "car", "bus", "truck"]
         self.predictions = ["decoy", "unsure", "hofitzer"]
+
+        self.detection_area = detection_area
+        self.frame_size = frame_size
 
     def load_model(self, model_path):
         model = YOLO(model_path)
@@ -157,34 +161,39 @@ class DeepDetector:
             res = model.predict(array)
             pred = self.predictions[int(res)]
 
-
-            for _, custom_idx, _ in detected_custom_objects:
-                targets[custom_idx] = pred
+            if detected_custom_objects is not None and len(detected_custom_objects) == 0:
+                for _, custom_idx, _ in detected_custom_objects:
+                    targets[custom_idx] = pred
 
             return objects_counter, targets
 
 
     def draw(self, detected_objects, frame, names):
-        bboxes = []
-        if detected_objects is not None and len(detected_objects) > 0:
+        if frame is not None:
+            bboxes = []
+            if detected_objects is not None and len(detected_objects) > 0:
 
-            for bbox, idx, class_id in detected_objects:
-                name = None
-                x1, y1, x2, y2 = map(int, bbox)
-                сolor = (0, 255, 0)
+                for bbox, idx, class_id in detected_objects:
+                    name = None
+                    x1, y1, x2, y2 = map(int, bbox)
+                    сolor = (0, 255, 0)
 
-                if class_id in names.keys():
-                    name = names[int(class_id)]
+                    if class_id in names.keys():
+                        name = names[int(class_id)]
 
-                if name in self.feature_list:
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), сolor, 2)
-                if name == "target":
-                    bboxes.append(bbox)
+                    if name in self.feature_list:
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), сolor, 2)
+
+                    if name == "target":
+                        bboxes.append(bbox)
 
 
-            return frame, bboxes
+                return bboxes
+            else:
+                return None
+
         else:
-            return frame, None
+            return None
 
     def assign_status(self, frame, detected_targets, targets):
         if detected_targets is not None and len(detected_targets) > 0:
@@ -194,21 +203,22 @@ class DeepDetector:
                 status = "Unkown"
                 color = (111, 111, 111)
 
-                text = f"{idx}:object:{status}"
-                for key, value in targets.items():
-                    if key == idx:
-                        status = value
-                        text = f"{idx}:object:{status}"
+                if len(targets) > 0:
+                    text = f"{idx}:object:{status}"
+                    for key, value in targets.items():
+                        if key == idx:
+                            status = value
+                            text = f"{idx}:object:{status}"
 
-                        if status == "decoy":
-                            color = (0, 255, 0)
-                        elif status == "unsure":
-                            color = (255, 255, 0)
-                        elif status == "hofitzer":
-                            color = (0, 0, 255)
+                            if status == "decoy":
+                                color = (0, 255, 0)
+                            elif status == "unsure":
+                                color = (255, 255, 0)
+                            elif status == "hofitzer":
+                                color = (0, 0, 255)
 
-                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-                cv2.putText(frame, text, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                    cv2.putText(frame, text, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
             return frame
         else:
@@ -223,13 +233,12 @@ class DeepDetector:
                 for bbox in bboxes:
                     x1, y1, x2, y2 = map(int, bbox)
 
-                    new_x1 = max(0, x1 - (x2 - x1) // 2)
-                    new_y1 = max(0, y1 - (y2 - y1) // 2)
-                    new_x2 = min(frame.shape[1], x2 + (x2 - x1) // 2)
-                    new_y2 = min(frame.shape[0], y2 + (y2 - y1) // 2)
+                    new_x1 = x1 - self.detection_area
+                    new_y1 = y1 - self.detection_area
+                    new_x2 = x2 + self.detection_area
+                    new_y2 = y2 + self.detection_area
 
-                    # print(f"UPD frame: {int(new_x1)}, {int(new_y1)}, {int(new_x2)}, {int(new_y2)}")
-                    cv2.rectangle(frame, (int(new_x1), int(new_y1)), (int(new_x2), int(new_y2)), (0, 0, 0), 8)
+                    # cv2.rectangle(frame, (int(new_x1), int(new_y1)), (int(new_x2), int(new_y2)), (0, 0, 0), 8)
                     frame = frame[int(new_y1):int(new_y2), int(new_x1):int(new_x2)]
 
                 return frame
@@ -252,15 +261,14 @@ class DeepDetector:
                 max_x2 = max(x2s)
                 max_y2 = max(y2s)
 
-                min_x1 = min_x1-20
-                min_y1 = min_y1-20
-                max_x2 = max_x2+20
-                max_y2 = max_y2+20
+                min_x1 = min_x1-self.detection_area
+                min_y1 = min_y1-self.detection_area
+                max_x2 = max_x2+self.detection_area
+                max_y2 = max_y2+self.detection_area
                 
-                cv2.rectangle(frame, (int(min_x1), int(min_y1)), (int(max_x2), int(max_y2)), (0, 0, 0), 8)
-                # cv2.rectangle(frame, (int(new_x1), int(new_y1)), (int(new_x2), int(new_y2)), (0, 0, 0), 8)
+                # cv2.rectangle(frame, (int(min_x1), int(min_y1)), (int(max_x2), int(max_y2)), (0, 0, 0), 8)
                 frame = frame[int(min_y1):int(max_y2), int(min_x1):int(max_x2)]
-                # frame = frame[int(new_y1):int(new_y2), int(new_x1):int(new_x2)]
+
 
             return frame
 
@@ -268,18 +276,52 @@ class DeepDetector:
 
             return frame
 
-    def display_objects(self, counter, frame):
+    def display_objects(self, counter, frame, targets):
+
+        decoys = []
+        unsure_objects = []
+        targets_objects = []
+
+        decoys_amount = 0
+        unsure_amount = 0
+        targets_amount = 0
+
+        if len(targets) > 0:
+            for key, value in targets.items():
+                if value == "decoy":
+                     decoys.append(key)
+                if value == "unsure":
+                    unsure_objects.append(key)
+                if value == "hofitzer":
+                    targets_objects.append(key)
+
+
+            decoys_amount = len(decoys)
+            unsure_amount = len(unsure_objects)
+            targets_amount = len(targets_objects)
+
+
+
         person_amount = len(counter["person"])
         car_amount = len(counter["car"])
         bus_amount = len(counter["bus"])
         truck_amount = len(counter["truck"])
 
-        cv2.rectangle(frame, (0, 0), (90, 40), (0, 0, 0), 80)
+        # frame.shape[0] - height, so y
+        # frame.shape[1] - width, so x
 
-        cv2.putText(frame, str(person_amount), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
-        cv2.putText(frame, str(car_amount), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
-        cv2.putText(frame, str(bus_amount), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
-        cv2.putText(frame, str(truck_amount), (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+        cv2.rectangle(frame, (0, 0), (int(frame.shape[1] * 0.32), int(frame.shape[0] * 0.37)), (0, 0, 0), cv2.FILLED)
+
+        cv2.putText(frame, f"Personal: {str(person_amount)}", (int(frame.shape[1] * 0.02), int(frame.shape[0] * 0.08)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(frame, f"Cars: {str(car_amount)}", (int(frame.shape[1] * 0.02), int(frame.shape[0] * 0.16)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(frame, f"Busses: {str(bus_amount)}", (int(frame.shape[1] * 0.02), int(frame.shape[0] * 0.24)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(frame, f"Trucks: {str(truck_amount)}", (int(frame.shape[1] * 0.02), int(frame.shape[0] * 0.32)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+        cv2.rectangle(frame, (0,  frame.shape[0]), (int(frame.shape[1] * 0.32), int(frame.shape[0] - frame.shape[0] * 0.37)), (0, 0, 0), cv2.FILLED)
+
+        cv2.putText(frame, f"Decoys: {str(decoys_amount)}", (int(frame.shape[1] * 0.02), int(frame.shape[0] * 0.72)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(frame, f"Unsure: {str(unsure_amount)}", (int(frame.shape[1] * 0.02), int(frame.shape[0] * 0.82)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(frame, f"Targets: {str(targets_amount)}", (int(frame.shape[1] * 0.02), int(frame.shape[0] * 0.92)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     def __call__(self):
         cap = cv2.VideoCapture(self.path)
@@ -287,15 +329,18 @@ class DeepDetector:
 
         while True:
             ret, frame = cap.read()
+            frame = cv2.resize(frame, self.frame_size)
             if not ret:
                 break
 
             results = self.results(self.model, frame)
 
             detected_objects = self.get_results(results, self.custom_tracker, frame)
-            _, bboxes = self.draw(detected_objects, frame, self.names)
+            bboxes = self.draw(detected_objects, frame, self.names)
 
             detected_yolo_objects = None
+            upd_frame = None
+
 
             if bboxes is not None:
                 upd_frame = self.detect_objects(frame, bboxes)
@@ -305,11 +350,10 @@ class DeepDetector:
                     detected_yolo_objects = self.get_results(yolo_results, self.yolo_tracker, upd_frame)
 
             counter, targets = self.count_objects(detected_objects, detected_yolo_objects, self.yolo_names)
-            frame, _ = self.draw(detected_yolo_objects, frame, self.yolo_names)
+            _ = self.draw(detected_yolo_objects, upd_frame, self.yolo_names)
             frame = self.assign_status(frame, detected_objects, targets)
 
-            # print(counter)
-            self.display_objects(counter, frame)
+            self.display_objects(counter, frame, targets)
             cv2.imshow('YOLO Tracker', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -323,7 +367,7 @@ device = "mps" if torch.backends.mps.is_available() else "cpu"
 model_path = "/Users/maxkucher/opencv/howitzer_detector/deepsort/artillery_detecor.pt"
 yolo_model_path = "yolo11l.pt"
 
-tracker = DeepDetector(path, device, 0.4, model_path, yolo_model_path)
+tracker = DeepDetector(path, device, 0.4, 30, (640, 320), model_path, yolo_model_path)
 tracker()
 
 
