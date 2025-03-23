9c733161a199c9cf26daa302e7b4643b8c3acbc4
Maks6666 <maxim.kucher2005@gmail.com>
2025-03-23 15:48:14 +0200

    DeepSort implementation

--- sort/main.py
+++ sort/main.py
@@ -86,6 +86,50 @@ class Tracker:
             return frame
 
 
+    def detection_area(self, frame, detections):
+        if detections is not None:
+            if len(detections) == 1:
+                for detection in detections:
+                    bbox, idx, class_id = detection
+                    x1, y1, x2, y2 = map(int, bbox)
+
+                    new_x1 = x1 - 40
+                    new_y1 = y1 - 40
+                    new_x2 = x2 + 40
+                    new_y2 = y2 + 40
+
+                    upd_frame = frame[new_y1:new_y2, new_x1:new_x2]
+                    cv2.rectangle(frame, (new_x1, new_y1), (new_x2, new_y2), (0, 255, 0), 2)
+
+            elif len(detections) > 1:
+
+                x1s, y1s, x2s, y2s = [], [], [], []
+                for detection in detections:
+
+                    bbox, idx, class_id = detection
+                    x1, y1, x2, y2 = map(int, bbox)
+
+                    x1s.append(x1)
+                    y1s.append(y1)
+                    x2s.append(x2)
+                    y2s.append(y2)
+
+                min_x1 = min(x1s)-40
+                min_y1 = min(y1s)-40
+                max_x2 = max(x2s)+40
+                max_y2 = max(y2s)+40
+
+                upd_frame = frame[min_y1:max_y2, min_x1:max_x2]
+                cv2.rectangle(frame, (min_x1, min_y1), (max_x2, max_y2), (0, 255, 0), 2)
+
+
+
+
+
+                ...
+        return None
+
+
     def __call__(self):
         cap = cv2.VideoCapture(self.path)
         assert cap.isOpened()
@@ -99,6 +143,7 @@ class Tracker:
             detections = self.get_results(results)
             detected_objects = self.detect(detections)
             frame = self.draw(frame, detected_objects, self.custom_names)
+            self.detection_area(frame, detected_objects)
 
             cv2.imshow('Video', frame)
 
